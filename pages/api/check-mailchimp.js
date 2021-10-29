78b7eacfb57754c513233a80905dc39ceb1698ed
Josh Suson <josh.suson@gmail.com>
2021-10-29 17:57:28 -0400

    Working on mailchimp integration

--- pages/api/check-mailchimp.js
+++ pages/api/check-mailchimp.js
@@ -1,4 +1,5 @@
 import mailchimp from "@mailchimp/mailchimp_marketing";
+import md5 from "md5";
 
 export default async function checkMailchimp(req, res) {
   mailchimp.setConfig({
@@ -6,6 +7,25 @@ export default async function checkMailchimp(req, res) {
     server: process.env.MAILCHIMP_SERVER_PREFIX,
   });
 
-  const response = await mailchimp.ping.get();
-  console.log(response);
+  const email = "josh.suson@gmail.com";
+  const subscriberHash = md5(email.toLowerCase());
+
+  let response;
+  let currentUser;
+
+  try {
+    response = await mailchimp.lists.getListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID,
+      subscriberHash
+    );
+    console.log(response.id);
+    currentUser = true;
+  } catch {
+    currentUser = false;
+  }
+
+  currentUser
+    ? console.log("Add to customer journey")
+    : console.log("Add to contacts first");
+  res.send("This worked");
 }
--- pages/api/log-entries.js
+++ pages/api/log-entries.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import mailchimp from "@mailchimp/mailchimp_marketing";
+import md5 from "md5";
 
 export default async function logEntries(req, res) {
   const { first_name, last_name, phone, email, zip_code, privacy_policy } =
     req.body;
+
+  const subscriberHash = md5(email.toLowerCase());
+  let currentUser;
 
   mailchimp.setConfig({
     apiKey: process.env.MAILCHIMP_API_KEY,
@@ -33,7 +37,31 @@ export default async function logEntries(req, res) {
     .then((data) => console.log(data.data.id))
     .catch((err) => console.error(err));
 
-  const response = await mailchimp.customerJourneys.trigger(
+  try {
+    const getContact = await mailchimp.lists.getListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID,
+      subscriberHash
+    );
+    console.log(getContact.id);
+    currentUser = true;
+  } catch {
+    console.log("There is no user");
+    currentUser = false;
+  }
+
+  if (!currentUser) {
+    console.log("Adding user");
+    const addContact = await mailchimp.lists.addListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID,
+      {
+        email_address: email,
+        status: "subscribed",
+      }
+    );
+    console.log(addContact.id);
+  }
+  console.log("Adding to customer journey");
+  const joinCustomerJourney = await mailchimp.customerJourneys.trigger(
     process.env.MAILCHIMP_JOURNEY_ID,
     process.env.MAILCHIMP_STEP_ID,
     {
@@ -41,7 +69,7 @@ export default async function logEntries(req, res) {
     }
   );
 
-  console.log(response);
+  console.log(joinCustomerJourney);
 
   console.log(req.body);
 
